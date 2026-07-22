f4161e982e2b8ed9532838132603018cdf824088
Syed Shahzeb <syed.shazeb90@gmail.com>
2026-07-23 03:28:26 +0500

    Stable version after Tauri desktop setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,27 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  clearScreen: false,
+
+  server: {
+    host: host || false,
+    port: 5173,
+    strictPort: true,
+
+    watch: {
+      ignored: (watchedPath: string) =>
+        /[\\/]src-tauri[\\/]/.test(watchedPath),
     },
   },
 });
